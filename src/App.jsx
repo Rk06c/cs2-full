939499a590824b71011fb0b6f8090374386b8379
Rk06c <rrk51608@gmail.com>
2025-07-13 20:47:02 +0530

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,24 @@
-import { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Header from './Header';
 import CourseComponent from './CourseComponent';
 import TechComponent from './TechComponent';
 import ContactComponent from './ContactComponent';
+//import DMOCurriculum from './DMOCurriculum';
+//import ADJPCurriculum from './ADJPCurriculum';
+import Header from './Header';
 import FooterComponent from './FooterComponent';
-import DMOCurriculum from './DMOCurriculum';
-import ADJPCurriculum from './ADJPCurriculum';
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-       
-        <Routes>
-          <Route path="/" element={
-            <>
-             <Header />
-              <CourseComponent />
-              <TechComponent />
-              <ContactComponent />
-              <FooterComponent />
-            </>
-          } />
-          <Route path="/dmo-curriculum" element={<DMOCurriculum />} />
-           <Route path="/adjp-curriculum" element={<ADJPCurriculum />} />
-        </Routes>
-        
-      </div>
-    </Router>
+    <>
+     <Header />
+      <CourseComponent />
+      <TechComponent />
+      <ContactComponent />
+      <FooterComponent />
+    </>
   );
 }
+
+
 
 export default App;
