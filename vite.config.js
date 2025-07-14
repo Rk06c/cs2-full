9d0af819a8dc977cf0c180be275e2287b235f3fe
Rk06c <rrk51608@gmail.com>
2025-07-14 13:30:06 +0530

    Update: third

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/cs2-full/', // ⚠️ Your repo name with slashes
+  base: '/', // ⚠️ Your repo name with slashes
   plugins: [react()],
 })
